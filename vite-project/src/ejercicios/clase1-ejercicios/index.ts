1fa479d1d91a40619b37afc84439c1e644650056
Cristian Infante <desarrollo.software@alullanos.com>
2025-09-20 11:55:17 -0500

    Clase 1

---
+++ vite-project/src/ejercicios/clase1-ejercicios/index.ts
@@ -0,0 +1,36 @@
+﻿// Ejercicio: implementar la interfaz para lo siguiente
+interface IUsuario{
+    id: number,
+    nombre: string,
+    email: string,
+    activo: boolean,
+    roles: string[],
+    direccion: {
+        ciudad: string,
+        pais: string
+    },
+    saludar(): string,
+    actualizarEmail: (nuevoEmail: string) => void
+}
+
+const usuario1: IUsuario = {
+    id: 1,
+    nombre: "Ana Pérez",
+    email: "ana@example.com",
+    activo: true,
+    roles: ["admin", "editor"],
+    direccion: {
+        ciudad: "Bogotá",
+        pais: "Colombia"
+    },
+    saludar() {
+        return `Hola, mi nombre es ${this.nombre}`;
+    },
+    actualizarEmail(nuevoEmail: string) {
+        this.email = nuevoEmail;
+    }
+}
+
+console.log(usuario1);
+
+export {};
